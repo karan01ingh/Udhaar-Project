031d2c740972701a23aa4e7154fdabac3f8145e8
Karan Singh Manral <128299796+karan01ingh@users.noreply.github.com>
2025-07-20 16:46:31 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,19 +1,8 @@
-// import { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './App.jsx';
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
-import axios from 'axios';
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'https://udhaar-project.onrender.com';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
