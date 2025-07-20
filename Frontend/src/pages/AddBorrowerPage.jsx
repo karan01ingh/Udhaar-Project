b3f7de7b0921c8a6910c4bb68b3eb0e95cdc5146
Karan Singh Manral <128299796+karan01ingh@users.noreply.github.com>
2025-07-20 17:15:43 +0530

    Update AddBorrowerPage.jsx

--- Frontend/src/pages/AddBorrowerPage.jsx
+++ Frontend/src/pages/AddBorrowerPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from "..utils/axios";
+import axios from "../utils/axios";
 import { toast } from 'react-hot-toast';
 export default function AddBorrowerPage() {
   const navigate = useNavigate();
