bfe74af19fcf7c40a15a15742b692e3e916df439
Karan Singh Manral <128299796+karan01ingh@users.noreply.github.com>
2025-07-20 16:06:42 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -11,7 +11,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
-
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'https://udhaar-project.onrender.com';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
