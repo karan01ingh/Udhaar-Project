e7987f17b8abc779dceed494fa3870ec544ac791
Karan Singh Manral <128299796+karan01ingh@users.noreply.github.com>
2025-07-20 16:59:15 +0530

    Update UpdateMoney.jsx

--- Frontend/src/pages/UpdateMoney.jsx
+++ Frontend/src/pages/UpdateMoney.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import axios from "..utils/axios";
 import { toast } from 'react-hot-toast';
 
 export default function UpdateAmountPage() {
