cb34d925238520211026f6078b1c78f8fa81bc38
Karan Singh Manral <128299796+karan01ingh@users.noreply.github.com>
2025-07-22 09:19:33 +0530

    Update axios.js

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://udhaar-project.onrender.com/api';
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 export default axios;
